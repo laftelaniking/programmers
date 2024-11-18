1acc1df77dc03f82c101f69b65602b186e9ace08
laftelaniking <159397464+laftelaniking@users.noreply.github.com>
2024-11-18 19:59:15 +0900

    [level 1] Title: 덧칠하기, Time: 1746.79 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -4,9 +4,8 @@ def solution(n, m, section):
         answer = len(section)
         return answer
     
-    #shallow copy
     section_copy= set(section[:])
-    
+
     for i in section:
         if i in section_copy:
             section_copy = section_copy - {j for j in range(i, i+m)}
