5a5b8fc5433fab367a9ea4f3b5bc26c673dd61e3
laftelaniking <159397464+laftelaniking@users.noreply.github.com>
2026-03-10 10:07:55 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,20 +1,6 @@
 function solution(s) {
-    var answer = 0;
-    var sign = 1;
-    console.log(s)
-    //s가 -+를 포함하고 있을때, 제거하고 -였으면 >> -1곱 
-    // console.log(s.indexOf('-'))
-    if(s.indexOf('-')!=-1){
-        answer = s.replace('-','');
-        console.log('-제거됨')
-        sign = -1;
-    }else if(s.indexOf('+')!=-1){
-        answer = s.replace('+','');
-        console.log('+제거됨')
-    }else{
-        answer = s
-    }
-    answer = answer * sign
+    var answer = s*1;
+    
     
     return answer;
 }
