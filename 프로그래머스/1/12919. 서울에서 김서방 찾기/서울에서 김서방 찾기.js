6125534b60b03fa06ddc1278ed10e1e4bf4df63f
laftelaniking <159397464+laftelaniking@users.noreply.github.com>
2026-03-23 07:58:27 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,8 @@
+function solution(seoul) {
+    var answer = '';
+    //배열에 kim의 인덱스를 반환한다
+    answer = seoul.indexOf('Kim');
+    // console.log(`"김서방은 ${answer}에 있다`);
+    answer = "김서방은 "+answer+"에 있다"
+    return answer;
+}
