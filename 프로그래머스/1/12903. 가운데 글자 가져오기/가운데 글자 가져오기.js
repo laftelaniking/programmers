7bbb85ac53aeccf636e1b963176f1cd9b8a60d5c
laftelaniking <159397464+laftelaniking@users.noreply.github.com>
2026-03-29 17:50:32 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,18 +1,12 @@
 function solution(s) {
-    //1을 2로 나누었을 때 나머지는 1 몫은 0
-    //3을 2로 나누었을 때 나머지 1 몫은 1
-    //5을 2로 나누었을 때 나머지 1 몫은 2
-    //즉, 문자의 길이가 홀수면 몫으로
-    // 짝수면 몫-1, 몫
     var answer = '';
-    var mok = parseInt(s.length/2)
-
-    if(s.length%2==1){ //짝수
-        answer = s[mok]
-    }else{//홀수
-        answer = s[mok-1]
+    var mok = parseInt(s.length/2);
+    // console.log(s[0]);
+    if(s.length%2!=0){
         answer += s[mok]
+    }else{
+        answer += s[mok-1] + s[mok];
     }
-    console.log(answer)
+    // console.log(answer)
     return answer;
 }
